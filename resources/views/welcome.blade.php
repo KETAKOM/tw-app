dcd90c71927a03e172dafe94efc90ac7e1311424
ketakom <ketakom2111178@gmail.com>
2019-01-12 20:17:00 +0900

    vuejs, typescriptを導入

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -64,7 +64,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <!-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -93,6 +93,10 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+        </div>  -->
+        <div id="app">
+            <example-component></example-component>
         </div>
+        <script lang="ts" src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
